31329c1ae17f80d7c5a2aa6dcc28dba5eeb57ee5
Jeremy Boggs <jeremy@clioweb.org>
2014-09-19 12:02:21 -0400

    Somewhat simplifies directory stuff.

--- j/plugins.js
+++ j/plugins.js
@@ -1,57 +1,34 @@
-  $('document').ready(function(){
-        // hides filter dropdowns on students page
-        $('#student-program-list').hide();
-        $('#student-research-areas').hide();
-        //event handlers to show filter dropdowns on students page
-        $('#program-dropdown').on('click', function(){
-            $('#student-program-list').toggle();
-        });
-        $('#research-area-dropdown').on('click', function(){
-            $('#student-research-areas').toggle();
-        });
-        // select container for isotope instance
-        var institutions = $('#program-list');
-        // initialize isotope instance with options
-        institutions.isotope(
-          {
-            itemSelector: '.program',
-            layoutMode: 'masonry',
-            getSortData: {
-                name: '[data-name]'
-            },
-            sortBy: 'name'
-          }
-        );
-        // script for button click filters
-        $('#institution-filters').on('click', 'a', function(){
-            var filterValue = $( this ).attr('data-filter');
-            institutions.isotope({ filter: filterValue });
-            //removes .is-checked from other buttons
-            $('#institution-filters').find('a').removeClass('is-checked');
-            //adds .is-checked to current filter button
-            $(this).addClass('is-checked');
-        });
+$('document').ready(function(){
 
-         // select container for isotope instance
-        var students = $('#student-list');
-        // initialize isotope instance with options
-        students.isotope(
-          {
-            itemSelector: '.student',
+    // hides filter dropdowns on students page
+    $('#filters ul').hide();
+
+    // select container for isotope instance
+    var things = $('#list');
+
+    // initialize isotope instance with options
+    things.isotope(
+        {
+            itemSelector: 'div',
             layoutMode: 'masonry',
             getSortData: {
                 name: '[data-name]'
             },
             sortBy: 'name'
-          }
-        );
-        // script for button click filters
-        $('#student-filters').on('click', 'a', function(){
-            var filterValue = $( this ).attr('data-filter');
-            students.isotope({ filter: filterValue });
-            // removes .is-checked from other buttons
-            $('#student-filters').find('a').removeClass('is-checked');
-            //adds .is-checked to current filter button
-            $(this).addClass('is-checked');
-        });
+        }
+    );
+
+    // script for button click filters
+    $('#filters').on('click', 'a', function(){
+
+        $(this).nextAll().toggle();
+
+        var filterValue = $( this ).attr('data-filter');
+        things.isotope({ filter: filterValue });
+        //removes .is-checked from other buttons
+        $('#filters').find('a').removeClass('is-checked');
+        //adds .is-checked to current filter button
+        $(this).addClass('is-checked');
     });
+
+});
